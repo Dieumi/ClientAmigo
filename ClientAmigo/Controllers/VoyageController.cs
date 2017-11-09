64bd03625df618127f04f351982fc6f25c26c40d
Dieumi <oxeyo@hotmail.fr>
2017-11-08 19:12:41 -0500

    creation voyage

--- ClientAmigo/Controllers/VoyageController.cs
+++ ClientAmigo/Controllers/VoyageController.cs
@@ -9,13 +9,42 @@ namespace ClientAmigo.Controllers
 {
     public class VoyageController : Controller
     {
+        private Voyage voyage = new Voyage();
         private Ville ville = new Ville();
-       
+        private List<Ville> listv = new List<Ville>();
         // GET: Voyage
         public ActionResult Index()
         {
             string listville = ville.getAllville();
+            var ser = new System.Web.Script.Serialization.JavaScriptSerializer();
+            listv= ser.Deserialize<List<Ville>>(listville);
+            ViewBag.listv = listv;
+            
             return View();
+        }
+        public ActionResult create()
+        {
+            string listville = ville.getAllville();
+            var ser = new System.Web.Script.Serialization.JavaScriptSerializer();
+            listv = ser.Deserialize<List<Ville>>(listville);
+            ViewBag.listv = listv;
+            Console.WriteLine(Session["id"]);
+            int nbplace = Convert.ToInt32(Request.Form["nbplace"]);
+            string dep = Request.Form["depart"];
+            string arr = Request.Form["arrive"];
+            string type = Request.Form["type"];
+            double prix = Convert.ToDouble(Request.Form["prix"]);
+            string date = Request.Form["date"];
+            string heure = Request.Form["heure"];
+            string id = Convert.ToString(Session["id"]);
+
+            var result = voyage.createVoyage(id, arr, dep, heure, date, nbplace, prix, type);
+
+            if (result !="")
+            {
+                ViewData["msg"] = "Voyage Crée";
+            }
+            return View("index");
         }
     }
 }
--- ClientAmigo/Models/Voyage.cs
+++ ClientAmigo/Models/Voyage.cs
@@ -1,40 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 using System.Net;
 using System.Text;
-using System.Web;
-using System.Web.Mvc;
+using System.IO;
 
 namespace ClientAmigo.Models
 {
-    public class Ville
+    public class Voyage
     {
-        public string id { get; set; }
-        public string name { get; set; }
-        public string region { get; set; }
-        private string PostData { get; set; }
-        private string page = "http://localhost:8090/ville";
-        public string getAllville()
+       
+        public string PostData { get; set; }
+        private string page = "http://localhost:8090/voyage";
+
+      
+        public string createVoyage(string id,string arrive, string depart, string heure, string date,int nbplace,double prix,string typevoiture)
         {
             var request = (HttpWebRequest)WebRequest.Create(page);
 
-            request.Method = HttpVerbs.Get.ToString();
+            request.Method = HttpVerbs.Post.ToString();
             request.ContentLength = 0;
             request.ContentType = "application/json";
-           
-            var encoding = new UTF8Encoding();
-         
+            PostData = "{ \"idUser\":\"" + id + "\",\"nbplace\":\"" + nbplace + "\",\"depart\":\"" + depart + "\",\"arrive\":\"" + arrive + "\",\"typeVoiture\":\""+typevoiture+ "\",\"price\":\"" + prix + "\",\"heureDep\":\"" + heure + "\",\"date\":\"" + date + "\"}";
 
-            
+            var encoding = new UTF8Encoding();
+            var bytes = Encoding.GetEncoding("iso-8859-1").GetBytes(PostData);
+            request.ContentLength = bytes.Length;
+
+            using (var writeStream = request.GetRequestStream())
+            {
+                writeStream.Write(bytes, 0, bytes.Length);
+            }
 
 
             using (var response = (HttpWebResponse)request.GetResponse())
             {
                 var responseValue = string.Empty;
 
-                if (response.StatusCode != HttpStatusCode.OK)
+                if (response.StatusCode != HttpStatusCode.Created)
                 {
                     var message = String.Format("Request failed. Received HTTP {0}", response.StatusCode);
                     throw new ApplicationException(message);
